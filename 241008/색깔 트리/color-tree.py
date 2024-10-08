5fd908bdf1a5aaa5d50dcda69545200814a765a4
HyeonjinKim <67854851+ChickenRushKR@users.noreply.github.com>
2024-10-08 15:18:27 +0900

    Title: 색깔 트리, Time: 9597ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 241008/색깔 트리/color-tree.py
+++ 241008/색깔 트리/color-tree.py
@@ -1,9 +1,10 @@
+nodes = []
+
 class node:
     def __init__(self,  m_id, p_id, color, max_depth):
         self.m_id = m_id
         self.p_id = p_id
         self.color = color
-        self.colors = []
         self.max_depth = max_depth
         self.parent = None
         self.heads = []
@@ -11,34 +12,40 @@ class node:
         self.childs = []
 
     def search(self, m_id):
-        if len(self.heads) == 0:
-            return None
-        else:
-            headidx = -1
-            for idx, m_id_in_head in enumerate(self.m_id_in_heads):
-                if m_id in m_id_in_head:
-                    headidx = idx
+        global nodes
+        for node_ in nodes:
+            if node_.m_id == m_id:
+                return node_
+        # if len(self.heads) == 0:
+        #     return None
+        # else:
+        #     headidx = -1
+        #     for idx, m_id_in_head in enumerate(self.m_id_in_heads):
+        #         if m_id in m_id_in_head:
+        #             headidx = idx
 
-            queue = [self.heads[headidx]]
-            while len(queue) != 0:
-                ptr = queue.pop(0)
-                if ptr.m_id == m_id:
-                    break
-                else:
-                    for c in ptr.childs:
-                        queue.append(c)
+        #     queue = [self.heads[headidx]]
+        #     while len(queue) != 0:
+        #         ptr = queue.pop(0)
+        #         if ptr.m_id == m_id:
+        #             break
+        #         else:
+        #             for c in ptr.childs:
+        #                 queue.append(c)
 
-        return headidx, ptr
+        # return headidx, ptr
 
     def insert_node(self, m_id, p_id, color, max_depth):
+        global nodes
         if p_id == -1:
             newnode = node(m_id, p_id, color, max_depth)
             self.heads.append(newnode)
             self.m_id_in_heads.append([m_id])
-            # self.colors.append(color)
+            nodes.append(newnode)
             return 1
         else:
-            headidx, target = self.search(p_id)
+            # headidx, target = self.search(p_id)
+            target = self.search(p_id)
             newnode = node(m_id, p_id, color, max_depth)
             newnode.parent = target
             ptr = target
@@ -53,13 +60,15 @@ class node:
                     ptr = ptr.parent
             if depth != -1:
                 newnode.parent.childs.append(newnode)
-                self.m_id_in_heads[headidx].append(m_id)
+                nodes.append(newnode)
+                # self.m_id_in_heads[headidx].append(m_id)
                 return 1
             else:
                 return 0
 
     def color_change(self, m_id, color):
-        headidx, target = self.search(m_id)
+        # headidx, target = self.search(m_id)
+        target = self.search(m_id)
         queue = [target]
         while len(queue) != 0:
             ptr = queue.pop(0)
@@ -72,7 +81,8 @@ class node:
         return 0
         
     def color_view(self, m_id):
-        headidx, target = self.search(m_id)
+        # headidx, target = self.search(m_id)
+        target = self.search(m_id)
         print(target.color)
 
     def score_view(self):
@@ -86,14 +96,14 @@ class node:
                 colors = []
                 while len(subtreeq) != 0:
                     ptr2 = subtreeq.pop(0)
-                    # if ptr2.color not in colors:
-                    colors.append(ptr2.color)
+                    if ptr2.color not in colors:
+                        colors.append(ptr2.color)
                     if len(ptr2.childs) == 0:
                         continue
                     else:
                         for c in ptr2.childs:
                             subtreeq.append(c)
-                score += (len(list(set(colors))) ** 2)
+                score += (len(colors) ** 2)
 
                 if len(ptr.childs) == 0:
                     continue
